bdc1ebefb9c296556b9642f6af69b9901939a9af
Artemy Markovsky <a.markovskiy@pixelplex.io>
2021-05-21 00:29:36 +0300

    Update docs

--- SharedFilesModule/SharedFilesModule/Classes/BuisnessLayer/Core/Implementation/ProtectedStorageCore.swift
+++ SharedFilesModule/SharedFilesModule/Classes/BuisnessLayer/Core/Implementation/ProtectedStorageCore.swift
@@ -6,9 +6,6 @@
 //  Copyright © 2021. All rights reserved.
 //
 
-// MARK: Для общения с защищенным хранилищем чезез пин (походу не прокатит так как StorageCore чуток иначе получается)
-// возможно надо моменять на подобие взяимодействия с флоу работы валюты. пока чуток хз
-
 public final class ProtectedStorageCore: StorageCore {
     public var storage: KeyValueStoring = ProtectedStorage()
 }
